130fbe1df2896c90c3b3c73a60d61b9440b60cf7
Andriy Chaika <andriy@chaika.in.ua>
2014-09-24 20:18:28 +0300

    #299 View loader

---
+++ ErrorHandler/index.js
@@ -0,0 +1,11 @@
+exports.configureModules = function(app) {
+    console.log("Error Handler: Configure Modules");
+}
+
+exports.configureRouters = function(app) {
+    console.log("Error Handler: Configure Routers");
+}
+
+exports.configureErrorHandlers = function(app) {
+    console.log("Error Handler: Configure Error Handlers");
+}
