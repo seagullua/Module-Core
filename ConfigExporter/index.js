0156b483bc99e568fe68b835b719dea112153702
Andriy Chaika <andriy@chaika.in.ua>
2014-09-25 20:24:44 +0300

    #300 Config middleware refactor

--- ConfigExporter/index.js
+++ ConfigExporter/index.js
@@ -2,6 +2,12 @@
     Module gives access for config variable into all templates
  */
 var Config = include('Core/Config');
+var path = require('path');
+
 exports.configureModules = function(app) {
     app.locals.config = Config;
+    app.locals.url_server = Config.server.url;
+    app.locals.url_content = Config.server.urlcontent;
+    console.log("aaaa");
+    app.locals.url_cont = function(){};
 }
