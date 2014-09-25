fbbbc3eda3f711153df5740da369b7f9ddb8d4bd
Andriy Chaika <andriy@chaika.in.ua>
2014-09-25 18:03:35 +0300

    #299 Layout system

--- ErrorHandler/index.js
+++ ErrorHandler/index.js
@@ -3,11 +3,15 @@ var res = express.response;
 var req = express.response;
 
 var ME = include('Core/ErrorHandler');
+var Config = include('Core/Config');
 
-
-res.showError = function(error_code) {
-    this.status(error_code).render(ME.views('error_page'), {
-        error_code: error_code
+res.showError = function(error_code, err) {
+    if(Config.production) {
+        err = undefined;
+    }
+    this.status(error_code).render(ME.view('error_page'), {
+        error_code: error_code,
+        err: err
     });
 };
 req.showError = res.showError;
@@ -34,7 +38,7 @@ function fatalErrorHandler(err, req, res, next)
 {
     console.error(err.stack);
     if(res.showError) {
-        res.showError(500);
+        res.showError(500, err);
     } else {
         res.send(500, err.stack);
     }
