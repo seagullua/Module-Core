bb773de54b8812d059bd489da1d6ec88992bc0de
Andriy Chaika <andriy@chaika.in.ua>
2014-10-30 19:16:41 +0200

    #308 frontend tests

--- Urls/index.js
+++ Urls/index.js
@@ -1,9 +1,19 @@
+var Config = include('Core/Config');
 var __urls = {
     addUrl: function(name, func) {
         this[name] = func;
     },
     configureModules: function(app) {
         app.locals.urls = include('Core/Urls');
+    },
+
+    /**
+     * Creates the full url from short
+     * for example "/book/" -> "http://localhost/book/"
+     * @param short_url
+     */
+    getFullUrl: function(short_url) {
+        return Config.server.url + short_url;
     }
 };
 
