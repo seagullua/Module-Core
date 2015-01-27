0be737b09028293bc73bc8ebb8895e9d40d4153d
garfieldua <andrey.davydenko@gmail.com>
2015-01-27 21:37:38 +0200

    #374 add new user fields + RU locale

--- User/db/User.js
+++ User/db/User.js
@@ -6,6 +6,8 @@ var userSchema = new Schema( {
     surname: {type: String, default: "Surname"},
     email:      {type: String,  match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,  unique: true, required: true, index: true},
     password:   {type: String, required: true},
+    publishing_house :    {type: String},
+    phone_number:         {type: String},
 
     is_email_confirmed: {type: Boolean, default: false}
 });
